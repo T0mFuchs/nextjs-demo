2ad343040ce73868cbb072f74bc99503d6bdd5e6
t0mfuchs <tf0x70m@gmail.com>
2022-11-28 16:25:37 +0100

    :)

---
+++ src/components/radix-ui/Separator/index.tsx
@@ -0,0 +1,18 @@
+import * as S from "@radix-ui/react-separator";
+
+import css from "./index.module.scss";
+
+export default function Separator({
+  className,
+  style,
+  orientation,
+  ...props
+}: {
+  className?: string;
+  style?: React.CSSProperties;
+  orientation?: "horizontal" | "vertical";
+}) {
+  return (
+    <S.Root className={`${css.root} ${className}`} style={style} {...props} />
+  );
+}
